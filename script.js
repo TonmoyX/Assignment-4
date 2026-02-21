7a0424b42a7a1f9402a6eff006113d7a53df5e5a
tonmoy-114 <spaceresearch2023@gmail.com>
2026-02-22 02:08:02 +0600

    most complete

--- script.js
+++ script.js
@@ -1,17 +1,195 @@
-const interList = [];
-const rejectList = [];
+let interList = [];
+let rejectList = [];
+let currentStatus = 'all';
 
 const totalCount = document.getElementById("headTotal");
-const headInter = document.getElementById("head-Inter");
-const headReject = document.getElementById("head-Reject");
+const headInterCount = document.getElementById("headInter");
+const headRejectCount = document.getElementById("headReject");
 const mainCount = document.getElementById("main-count");
 
-const allCardSection = document.getElementById('allCards')
+const mainContainer = document.querySelector('main');
+
+const allCardSection = document.getElementById('allCards');
+const filterSection = document.getElementById('filter-section');
 
 function count(){
     totalCount.innerText = allCardSection.children.length;
     mainCount.innerText = allCardSection.children.length + " jobs";
-    headInter.innerText = interList.length;
-    headReject.innerText = rejectList.length;
+    headInterCount.innerText = interList.length;
+    headRejectCount.innerText = rejectList.length;
 }
 count();
+
+const allBtn = document.getElementById('all-btn');
+const interviewBtn = document.getElementById('interview-btn');
+const rejectBtn = document.getElementById('reject-btn');
+
+function toggle(id){
+      allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+      interviewBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+      rejectBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+
+      allBtn.classList.add('bg-white', 'text-[#64748B]')
+      interviewBtn.classList.add('bg-white', 'text-[#64748B]')
+      rejectBtn.classList.add('bg-white', 'text-[#64748B]')
+
+      const select = document.getElementById(id);
+      select.classList.add('bg-[#3B82F6]', 'text-white');
+      select.classList.remove('bg-white', 'text-[#64748B]');
+
+
+      if (id == 'interview-btn') {
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+        renderInter()
+    } else if (id == 'all-btn') {
+        allCardSection.classList.remove('hidden');
+        filterSection.classList.add('hidden')
+    } else if (id == 'reject-btn') {
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+        renderReject()
+    }
+
+  
+}
+mainContainer.addEventListener('click', function(e){
+    if(e.target.classList.contains('interBtn')){
+        const parenNode = e.target.parentNode.parentNode;
+        
+
+        const company = parenNode.querySelector('.companyName').innerText
+        const designation = parenNode.querySelector('.designation').innerText
+        const locationSalary = parenNode.querySelector('.locationSalary').innerText
+        const status = parenNode.querySelector('.status').innerText
+        const note = parenNode.querySelector('.note').innerText
+
+        parenNode.querySelector('.status').innerText = 'Interview';
+
+        const cardInfo = {
+            company, 
+            designation, 
+            locationSalary, 
+            status: 'Interview',
+            note
+        }
+        const companyExist = interList.find(item => item.company == cardInfo.company);
+        if(!companyExist){
+            interList.push(cardInfo);
+        }
+
+        rejectList = rejectList.filter(item => item.company != cardInfo.company);
+
+        //current status from toogle style
+          if (currentStatus == 'interview-btn') {
+            renderReject()
+        }
+        count();
+    }
+    else if(e.target.classList.contains('rejectBtn')){
+         const parenNode = e.target.parentNode.parentNode;
+
+        const company = parenNode.querySelector('.companyName').innerText
+        const designation = parenNode.querySelector('.designation').innerText
+        const locationSalary = parenNode.querySelector('.locationSalary').innerText
+        const status = parenNode.querySelector('.status').innerText
+        const note = parenNode.querySelector('.note').innerText
+
+        parenNode.querySelector('.status').innerText = 'Reject';
+
+        const cardInfo = {
+            company, 
+            designation, 
+            locationSalary, 
+            status: 'Reject',
+            note
+        }
+
+        const companyExist = rejectList.find(item => item.company == cardInfo.company);
+        if(!companyExist){
+            rejectList.push(cardInfo);
+        }
+
+        interList = interList.filter(item => item.company != cardInfo.company);
+
+        //current status from toogle style
+          if (currentStatus == 'reject-btn') {
+            renderInter()
+        }
+
+         count();
+    }
+})
+
+function renderInter(){
+     filterSection.innerHTML = '';
+     for (let inter of interList) {
+        console.log(inter);
+
+        let div = document.createElement('div');
+
+         div.className = 'card flex justify-between mt-4 p-8 bg-white rounded-[8px]'
+        div.innerHTML = `
+        <div>
+            <div>
+            <h1 class="companyName  text-[#002C5C] text-[26px] font-semibold">${inter.company}</h1>
+            <p class="designation  text-[#64748B] text-[22px]">${inter.designation}</p>
+          </div>
+            <h1 class="locationSalary  text-[#64748B] mt-[20px]">${inter.locationSalary}</h1>
+
+             <div>
+            <h1 class="status w-[113px] rounded-[4px] text-[14px] text-[#002C5C] font-medium bg-[#002C5C]/10 py-[8px] px-[12px] mt-[20px]">${inter.status}</h1>
+            <p class="note  mt-[8px] text-[#323B49]text-[1.1rem]">${inter.note}</p>
+        </div>
+
+
+        <div class="mt-[20px] flex gap-4">
+            <button class="interBtn border-1 text-[#10B981] font-semibold text-[14px] border-[#10B981] hover:animate-pulse py-2 px-4 rounded-[4px]">INTERVIEW</button>
+            <button class="rejectBtn border-1 text-[#EF4444] font-semibold text-[14px] border-[#EF4444] py-2 px-4 rounded-[4px] hover:animate-pulse">REJECTED</button>
+        </div>
+        </div>
+
+        <div>
+            <button>
+                <h1 class="hover:text-red-700 hover:border-red-700 bg-white p-1 rounded-full text-[1.5rem] text-[#64748B] border-2 border-[#64748B]"><i class="fa-regular fa-trash-can"></i></h1>
+            </button>
+        </div>`
+        filterSection.appendChild(div);
+}
+}
+
+function renderReject(){
+     filterSection.innerHTML = ''
+    // crating innerHtml
+    for (let reject of rejectList) {
+
+        let div = document.createElement('div');
+        div.className = 'card flex justify-between mt-4 p-8 bg-white rounded-[8px]'
+        div.innerHTML = `
+        <div>
+            <div>
+            <h1 class="companyName  text-[#002C5C] text-[26px] font-semibold">${reject.company}</h1>
+            <p class="designation  text-[#64748B] text-[22px]">${reject.designation}</p>
+          </div>
+            <h1 class="locationSalary  text-[#64748B] mt-[20px]">${reject.locationSalary}</h1>
+
+             <div>
+            <h1 class="status w-[113px] rounded-[4px] text-[14px] text-[#002C5C] font-medium bg-[#002C5C]/10 py-[8px] px-[12px] mt-[20px]">${reject.status}</h1>
+            <p class="note  mt-[8px] text-[#323B49]text-[1.1rem]">${reject.note}</p>
+        </div>
+
+
+        <div class="mt-[20px] flex gap-4">
+            <button class="interBtn border-1 text-[#10B981] font-semibold text-[14px] border-[#10B981] hover:animate-pulse py-2 px-4 rounded-[4px]">INTERVIEW</button>
+            <button class="rejectBtn border-1 text-[#EF4444] font-semibold text-[14px] border-[#EF4444] py-2 px-4 rounded-[4px] hover:animate-pulse">REJECTED</button>
+        </div>
+        </div>
+
+        <div>
+            <button>
+                <h1 class="hover:text-red-700 hover:border-red-700 bg-white p-1 rounded-full text-[1.5rem] text-[#64748B] border-2 border-[#64748B]"><i class="fa-regular fa-trash-can"></i></h1>
+            </button>
+        </div>`
+        filterSection.appendChild(div);
+}
+}
